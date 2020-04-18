2ad9ef251755b21cd1917518c5fd312dd381fd29
Fazlul Kabir Shohag <linkvisionsoft@yahoo.com>
2020-04-18 12:33:18 +0600

    update package

--- src/Mixins/ModelMixins/QueryMixin.php
+++ src/Mixins/ModelMixins/QueryMixin.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace Shohag\Mixins\ModelMixins;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
+namespace App\Mixins\ModelMixins;
 
 /**
  * @author Md.Shohag <Shohag.fks@gmail.com>
  */
 
-Trait QueryMixin {
+trait ProtectedQueryMixin
+{
 
     private $paramFilters = '';
+    private $uniqueTableList = [];
 
-     /**
+    /**
      * String separator
      * @param string $string
      * @param array 
@@ -32,32 +32,10 @@ Trait QueryMixin {
         return $filters;
     }
 
-    public function firstPatternMatching($string, $pattern): bool {
-        $len = strlen($string) - strlen($pattern);
-        $p_len = strlen($pattern);
-        for($i = 0; $i <= $len; $i++) {
-            $counter = 0;
-            for($j = $i; $j < $i+$p_len; $j++) {
-                if($string[$j] == $pattern[$counter]) {
-                    $counter++;
-                    if($counter == $p_len) return true;
-                    continue;
-                } else {
-                   break;
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * @return $tableName
-     */
     public static function getTableName()
     {
         return with(new static)->getTable();
     }
-
 
     /**
      * @param string $string string
@@ -85,118 +63,92 @@ Trait QueryMixin {
     }
 
     /**
-     * @param related $key
-     * @param string $field
-     * @param querySet $query
-     * @param relation $relations
-     * @return Query
-     */
-    public function joinQuery($relatedTo, $key, $value, $query, $relations)
-    {
-        foreach ($relations as $relation) {
-            if ($relatedTo === $relation['key']) {
-                $f_table_name = $relation['model']::getTableName();
-                $query = $query->join($f_table_name, $f_table_name.'.'.$relation['related'], '=', $this->tableName.'.'.$relation['self']);
-                $query->where($f_table_name.'.'.$key, $value);
-                break;
-            }
-        }
-        return $query;
-    }
-
-    /**
      * @param query $querySet
      * @return query
      */
+    public function joinQuery($field, $model){
+        if(method_exists($model, 'relations')){
+            if($this->firstPatternMatching($field, '__')) {
+                $field_keys = explode('__', $field);
+                $key = $field_keys[0];
+                $field = $field_keys[1];
+                foreach($model::relations() as $relation){ 
+                    if($key == $relation['key']) {
+                        $field = (new $relation['model'])->getTable().'.'.$field;
+                        return $field;
+                    }
+                }
+            } else {
+                return $this->getTableName().'.'.$field;
+            }
+        }
+        return $this->getTableName().'.'.$field;
+        
+    }
+
+    public function selectQueryField(){
+        $selectFields = [];
+        $selectFields[] = 'id';
+        $serializerFields = $this->PostSerializerFields();
+        foreach($serializerFields as $field) {
+            if(!$this->firstPatternMatching($field, '__')) {
+                $selectFields[] = $this->tableName.'.'.$field;
+            }
+        }
+        return $selectFields;
+    }
     public function appliedMullipleFilter($querySet, $filter)
     {
         $this->paramFilters = $filter;
         $queryFilters = $this->filterSeparator($this->paramFilters);
 
+        if(method_exists($this, 'relations')){
+            foreach($this::relations() as $relation){
+                $f_table_name = $relation['model']::getTableName();
+                $b_table_name = $relation['base_model']::getTableName();
+                $querySet->join($f_table_name, $f_table_name.'.'.$relation['related'], '=', $b_table_name.'.'.$relation['base_reation']);
+            }
+        }
         foreach ($queryFilters as $qFilter) {
-            if (strpos($qFilter['key'], 'b2n_') !== false) {
+            if (strpos($qFilter['key'], '__b2n') !== false) {
+                $keyMap = explode('__b2n', $qFilter['key']);
+                $key = $keyMap[0];
+                $valueMap = explode('~', $qFilter['value']);
 
-                $keyMap = explode('_', $qFilter['key']);
-                $key = $keyMap[1];
-                $valueMap = explode('-', $qFilter['value']);
-                $querySet->whereBetween($this->tableName.'.'.$key, [$valueMap[0], $valueMap[1]]);
-            } else {
-                if ($this->startsWith($qFilter['key'], 'like~')) {
-                    $len = strlen('like~');
-                    $key = substr($qFilter['key'], $len);
-                    $querySet->where($this->tableName.'.'.$key, 'like', '%' . $qFilter['value'] . '%');
-                } else {
-                    // Foreign Key filter apply
-                    if ($this->firstPatternMatching($qFilter['key'], '__')) {
-                        $key_relations = explode('__', $qFilter['key']);
-                        $prefix_key = $key_relations[0];
-                        $key = $key_relations[1];
-
-                        if (method_exists($this, 'relations')) {
-                            $querySet = $this->joinQuery($prefix_key, $key,  $qFilter['value'], $querySet, $this->relations());
-                        }
-                        $selectFields = [];
-                        $serializerFields = $this->SerializerFields();
-                        foreach($serializerFields as $field) {
-                            if(!$this->firstPatternMatching($field, '__')) {
-                                $selectFields[] = $this->tableName.'.'.$field;
-                            }
-                        }
-                        $querySet->select($selectFields);
-                    } else {
-                        $querySet->where($this->tableName.'.'.$qFilter['key'], $qFilter['value']);
-                    }
-                }
+                $key = $this->joinQuery($key, $this);
+                $querySet->whereBetween($key, [$valueMap[0], $valueMap[1]]);
+            } else if($this->firstPatternMatching($qFilter['key'], '__gte')) {
+                $keyMap = explode('__gte', $qFilter['key']);
+                $key = $keyMap[0];
+                $key = $this->joinQuery($key, $this);
+                $querySet->where($key, '>=', $qFilter['value']);
+            } 
+            else if($this->firstPatternMatching($qFilter['key'], '__lte')) {
+                $keyMap = explode('__lte', $qFilter['key']);
+                $key = $keyMap[0];
+                $key = $this->joinQuery($key, $this);
+                $querySet->where($key, '<=', $qFilter['value']);
+            } 
+            else if ($this->startsWith($qFilter['key'], 'like~')) {
+                $len = strlen('like~');
+                $key = substr($qFilter['key'], $len);
+                $key = $this->joinQuery($key, $this);
+                $searchData = strtolower($qFilter['value']);
+                $querySet->whereRaw('lower('.$key.') like (?)',["%{$searchData}%"]);
+            } 
+            else if ($this->startsWith($qFilter['key'], 'in~')) {
+                $len = strlen('in~');
+                $key = substr($qFilter['key'], $len);
+                $key = $this->joinQuery($key, $this);
+                $searchData = explode("--",$qFilter['value']);
+                $querySet->wherein($key, $searchData);
+            }
+            else {
+                $key = $qFilter['key'];
+                $key = $this->joinQuery($key, $this);
+                $querySet->where($key, $qFilter['value']);
             }
         }
         return $querySet;
-    }
-
-     /**
-     * @param int $resourcePerPage
-     * @param string $orderBy
-     * @return JsonResponse
-     * @throws \ReflectionException
-     */
-    function getAll($filter = null, $resourcePerPage = 10, $orderBy = 'DESC')
-    {
-        $querySet = $this->model::orderBy($this->tableName.'.id', $orderBy);
-        if($filter) {
-            $querySet = $this->appliedMullipleFilter($querySet, $filter);
-        }
-
-        return $querySet->paginate($resourcePerPage);
-    }
-
-    function getResourceById($id)
-    {
-        $resource = $this->model::find($id);
-        return $resource;
-    }
-
-    public function bulkCreate($request) {
-        $EntityModel = $this->model;
-        $bulks = $request->bulks;
-        $tableName =  $EntityModel->getTable();
-        $ids = [];
-        if(!empty($bulks)) {
-            DB::beginTransaction();
-            foreach($bulks as $resource) {
-                $ids[] = DB::table($tableName)->insertGetId($resource);
-            }
-            DB::commit();
-        }
-        if($ids) {
-            return response()->json(['data' => $EntityModel::whereIn('id', $ids)->get()], 200);
-  
-        } else {
-            return response()->json(['data' => []], 200);
-        }
-    }
-
-    public function validatorChecker($_request, $v_fields) {
-        $validator = Validator::make($_request, $v_fields);
-        if ($validator->fails()) return response()->json(['errors' => $validator->messages()]);
-        return false;
     }
 }
